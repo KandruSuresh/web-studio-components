15048a04c035cb4a905fd3f98552d8ac71f2194b
Suresh Kandru <kandru.sureshbabu@gmail.com>
2025-03-05 20:38:38 +0530

    module fedaration changes added

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,12 @@ import Button from './components/Button/Button'
 
 function App() {
 
-
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        
           Button Component:
           <Button name={'Login'}/>
-  
       </div>
     </>
   )
